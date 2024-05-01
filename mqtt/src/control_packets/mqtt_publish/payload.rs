0b763b87d131ef3f868f9a2c12e33d7c4694fd3e
GianLucaSpagnolo <gianluuba@gmail.com>
2024-05-01 15:02:50 -0300

    fix: all assets are set

--- mqtt/src/control_packets/mqtt_publish/payload.rs
+++ mqtt/src/control_packets/mqtt_publish/payload.rs
@@ -4,24 +4,24 @@ use crate::data_structures::data_types::data_representation::{
     read_two_byte_integer, read_utf8_encoded_string,
 };
 
-pub struct PublishPayload {
+pub struct _PublishPayload {
     pub bytes_length: u8,
     pub message: String,
 }
 
-impl PublishPayload {
-    pub fn length(&self) -> u8 {
+impl _PublishPayload {
+    pub fn _length(&self) -> u8 {
         self.bytes_length
     }
 
-    pub fn new(message: String) -> Self {
-        PublishPayload {
+    pub fn _new(message: String) -> Self {
+        _PublishPayload {
             bytes_length: message.len() as u8,
             message,
         }
     }
 
-    pub fn as_bytes(&self) -> Vec<u8> {
+    pub fn _as_bytes(&self) -> Vec<u8> {
         let mut bytes: Vec<u8> = Vec::new();
 
         bytes.extend_from_slice(&self.bytes_length.to_be_bytes());
@@ -30,11 +30,11 @@ impl PublishPayload {
         bytes
     }
 
-    pub fn read_from(stream: &mut dyn Read, length: u8) -> Result<Self, Error> {
+    pub fn _read_from(stream: &mut dyn Read, length: u8) -> Result<Self, Error> {
         let message_len = read_two_byte_integer(stream)?;
         let message = read_utf8_encoded_string(stream, message_len)?;
 
-        Ok(PublishPayload {
+        Ok(_PublishPayload {
             bytes_length: length,
             message,
         })
