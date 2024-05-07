f4c9c5e5f7b27d84cf56777a9495831cb82eae8e
tin-gprieto <mgonzalezp@fi.uba.ar>
2024-05-06 23:43:45 -0300

    fix: get address

--- app/src/server_example.rs
+++ app/src/server_example.rs
@@ -5,12 +5,8 @@ use mqtt::{config::ServerConfig, server::*};
 fn main() -> Result<(), Error>{
     let config = ServerConfig::from_file(String::from("app/files/server.txt"))?;
     
+    let addr = config.get_address();
     
-    let addr = config.socket_address.clone();
-    
-    println!("Config ip {:?}", addr);
-    println!("Config port {:?}", config.port);
-
     match Server::run(config) {
         Ok(_) => println!("Corriendo servidor en {:?}", addr),
         Err(e) => println!("Error en el server: {:?}", e),
