5ceba399b753e166524dc603822a332bec0e057d
rafaortegano <rortegano@fi.uba.ar>
2024-05-11 04:31:15 -0300

    fix: Disconnect packet refactored
    Co-authored-by: GianLucaSpagnolo <gianluuba@gmail.com>

--- mqtt/src/control_packets/mqtt_disconnect/disconnect.rs
+++ mqtt/src/control_packets/mqtt_disconnect/disconnect.rs
@@ -1,121 +1,132 @@
 use std::io::{Error, Read, Write};
 
-use super::variable_header::_DisconnectVariableHeader;
-use crate::control_packets::mqtt_packet::fixed_header::{PacketFixedHeader, _DISCONNECT_PACKET};
+use crate::control_packets::mqtt_packet::packet::generic_packet::*;
+use crate::control_packets::mqtt_packet::packet_properties::PacketProperties;
+
+use super::disconnect_properties::_DisconnectProperties;
+use crate::control_packets::mqtt_packet::{
+    fixed_header::{PacketFixedHeader, _DISCONNECT_PACKET},
+    packet::generic_packet::Serialization,
+};
 
 pub struct _Disconnect {
-    pub fixed_header: PacketFixedHeader,
-    pub variable_header: _DisconnectVariableHeader,
+    pub properties: _DisconnectProperties,
 }
 
-pub struct _DisconnectProperties {
-    pub session_expiry_interval: u32,
-    pub reason_string: String,
-    pub user_property: (String, String),
-    pub server_reference: String,
-}
+impl Serialization for _Disconnect {
+    fn read_from(stream: &mut dyn Read, _remaining_lenght: u16) -> Result<Self, Error> {
+        let properties = _DisconnectProperties::read_from(stream)?;
 
-impl _Disconnect {
-    pub fn _write_to(&self, stream: &mut dyn Write) -> Result<(), Error> {
-        let fixed_header = self.fixed_header.as_bytes();
-        stream.write_all(&fixed_header)?;
+        let disconnect = _Disconnect { properties };
 
-        let variable_header = self.variable_header._as_bytes();
-        stream.write_all(&variable_header)?;
+        Ok(disconnect)
+    }
+
+    fn write_to(&self, stream: &mut dyn Write) -> Result<(), Error> {
+        let properties = self.properties.as_bytes()?;
+
+        let remaining_length = self.properties.size_of();
+        let fixed_header = PacketFixedHeader::new(_DISCONNECT_PACKET, remaining_length);
+        let fixed_header_bytes = fixed_header.as_bytes();
+
+        stream.write_all(&fixed_header_bytes)?;
+        stream.write_all(&properties)?;
 
         Ok(())
     }
 
-    pub fn _read_from(stream: &mut dyn Read) -> Result<Self, Error> {
-        let fixed_header = PacketFixedHeader::read_from(stream)?;
-
-        let variable_header = _DisconnectVariableHeader::_read_from(stream)?;
-
-        let connect = _Disconnect {
-            fixed_header,
-            variable_header,
-        };
-        Ok(connect)
+    fn packed_package(package: _Disconnect) -> PacketReceived {
+        PacketReceived::Disconnect(Box::new(package))
     }
-    pub fn _new(
-        disconnect_reason_code: u8,
-        disconnect_properties: _DisconnectProperties,
-    ) -> Result<Self, Error> {
-        let variable_header =
-            _DisconnectVariableHeader::_new(disconnect_reason_code, disconnect_properties)?;
-        let remaining_length = variable_header._length();
-        let fixed_header = PacketFixedHeader::new(_DISCONNECT_PACKET, remaining_length);
-        Ok(_Disconnect {
-            fixed_header,
-            variable_header,
-        })
+}
+
+impl _Disconnect {
+    pub fn _new(properties: _DisconnectProperties) -> Self {
+        _Disconnect { properties }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::control_packets::mqtt_packet::packet_property::{
-        PacketProperty, REASON_STRING, SERVER_REFERENCE, SESSION_EXPIRY_INTERVAL,
-        USER_PROPERTY,
-    };
-
     use super::*;
 
     #[test]
     fn test_disconnect() {
-        let disconnect = _Disconnect::_new(
-            0,
-            _DisconnectProperties {
-                session_expiry_interval: 0,
-                reason_string: "reason".to_string(),
-                user_property: ("name".to_string(), "value".to_string()),
-                server_reference: "server".to_string(),
-            },
-        )
-        .unwrap();
+        let disconnect = _Disconnect::_new(_DisconnectProperties {
+            disconnect_reason_code: 0,
+            session_expiry_interval: Some(10),
+            reason_string: Some("reason".to_string()),
+            user_property: Some(("name".to_string(), "value".to_string())),
+            server_reference: Some("server".to_string()),
+        });
 
-        let mut buf = Vec::new();
-        disconnect._write_to(&mut buf).unwrap();
+        // ESCRIBE EL PACKET EN EL BUFFER
+        let mut buffer: Vec<u8> = Vec::new();
+        disconnect.write_to(&mut buffer).unwrap();
 
-        let mut buf = std::io::Cursor::new(buf);
-        let disconnect = _Disconnect::_read_from(&mut buf).unwrap();
+        // LEE EL PACKET DEL BUFFER
+        let mut buffer = buffer.as_slice();
+        let disconnect_fixed_header = PacketFixedHeader::read_from(&mut buffer).unwrap();
+        let disconnect =
+            _Disconnect::read_from(&mut buffer, disconnect_fixed_header.remaining_length).unwrap();
 
-        assert_eq!(disconnect.fixed_header.packet_type, _DISCONNECT_PACKET);
-        assert_eq!(disconnect.variable_header.disconnect_reason_code, 0);
+        assert_eq!(disconnect.properties.disconnect_reason_code, 0);
+        assert_eq!(disconnect.properties.variable_props_size(), 4);
 
-        let props = &disconnect.variable_header.properties;
+        let props = disconnect.properties;
 
-        if let PacketProperty::SessionExpiryInterval(value) =
-            props._get_property(SESSION_EXPIRY_INTERVAL).unwrap()
-        {
-            assert_eq!(*value, 0);
+        if let Some(value) = props.session_expiry_interval {
+            assert_eq!(value, 10);
         } else {
-            panic!("Error");
+            panic!("Invalid property");
         }
 
-        if let PacketProperty::ReasonString(value) =
-            props._get_property(REASON_STRING).unwrap()
-        {
-            assert_eq!(value, "reason");
+        if let Some(value) = props.reason_string {
+            assert_eq!(value, "reason".to_string());
         } else {
-            panic!("Error");
+            panic!("Invalid property");
         }
 
-        if let PacketProperty::UserProperty(value) =
-            props._get_property(USER_PROPERTY).unwrap()
-        {
-            assert_eq!(value.0, "name");
-            assert_eq!(value.1, "value");
+        if let Some(value) = props.user_property {
+            assert_eq!(value.0, "name".to_string());
+            assert_eq!(value.1, "value".to_string());
         } else {
-            panic!("Error");
+            panic!("Invalid property");
         }
 
-        if let PacketProperty::ServerReference(value) =
-            props._get_property(SERVER_REFERENCE).unwrap()
-        {
-            assert_eq!(value, "server");
+        if let Some(value) = props.server_reference {
+            assert_eq!(value, "server".to_string());
         } else {
-            panic!("Error");
+            panic!("Invalid property");
         }
+    }
+
+    #[test]
+    fn test_disconnect_empty_properties() {
+        let disconnect = _Disconnect::_new(_DisconnectProperties {
+            disconnect_reason_code: 1,
+            session_expiry_interval: None,
+            reason_string: None,
+            user_property: None,
+            server_reference: None,
+        });
+
+        // ESCRIBE EL PACKET EN EL BUFFER
+        let mut buffer: Vec<u8> = Vec::new();
+        disconnect.write_to(&mut buffer).unwrap();
+
+        // LEE EL PACKET DEL BUFFER
+        let mut buffer = buffer.as_slice();
+        let disconnect_fixed_header = PacketFixedHeader::read_from(&mut buffer).unwrap();
+        let disconnect =
+            _Disconnect::read_from(&mut buffer, disconnect_fixed_header.remaining_length).unwrap();
+
+        assert_eq!(disconnect.properties.disconnect_reason_code, 1);
+        assert_eq!(disconnect.properties.variable_props_size(), 0);
+
+        assert_eq!(disconnect.properties.session_expiry_interval, None);
+        assert_eq!(disconnect.properties.reason_string, None);
+        assert_eq!(disconnect.properties.user_property, None);
+        assert_eq!(disconnect.properties.server_reference, None);
     }
 }
