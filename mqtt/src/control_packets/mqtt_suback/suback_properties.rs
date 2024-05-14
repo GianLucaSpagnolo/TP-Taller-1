ce0cb114a15fdc42cb50fc11b9b55049381784dc
rafaortegano <rortegano@fi.uba.ar>
2024-05-14 13:34:34 -0300

    add: suback_propertoies read_from

--- mqtt/src/control_packets/mqtt_suback/suback_properties.rs
+++ mqtt/src/control_packets/mqtt_suback/suback_properties.rs
@@ -1,4 +1,7 @@
-use crate::control_packets::mqtt_packet::{packet_property::*, packet_properties::PacketProperties, variable_header_properties::VariableHeaderProperties};
+use crate::control_packets::mqtt_connect::payload;
+use crate::control_packets::mqtt_packet::reason_codes;
+use crate::control_packets::mqtt_packet::{packet_properties::PacketProperties, packet_property::*, variable_header_properties::VariableHeaderProperties};
+use crate::common::data_types::data_representation::*;
 
 use std::io::Error;
 
@@ -80,7 +83,37 @@ impl PacketProperties for _SubackProperties {
         Ok(bytes)
     }
 
-    fn read_from(stream: &mut dyn std::io::Read) -> Result<Self, std::io::Error> {
-        todo!()
+    fn read_from(stream: &mut dyn std::io::Read) -> Result<Self, Error> {
+        let packet_identifier = read_two_byte_integer(stream)?;
+
+        let variable_header_properties = VariableHeaderProperties::read_from(stream)?;
+
+        let mut reason_string = None;
+        let mut user_property = None;
+
+        for property in &variable_header_properties.properties {
+            match property.id() {
+                REASON_STRING => {
+                    reason_string = property.value_string();
+                }
+                USER_PROPERTY => {
+                    user_property = property.value_string_pair();
+                }
+                _ => {}
+            }
+        }
+        let mut reason_codes = Vec::new();
+
+        while let Some(reason_code) = Some(read_byte(stream)?) {
+            reason_codes.push(reason_code);
+        }
+
+        Ok(_SubackProperties {
+            packet_identifier,
+            reason_string,
+            user_property,
+            reason_codes,
+        })
+
     }
 }
