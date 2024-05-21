05bf6f8f728ff067f75b29cdc6ecbd55ed1e3d09
tin-gprieto <mgonzalezp@fi.uba.ar>
2024-05-21 14:55:53 -0300

    fix: dead code

--- mqtt/src/logger/actions.rs
+++ mqtt/src/logger/actions.rs
@@ -1,9 +1,5 @@
-use std::fmt::Debug;
-
-pub trait MqttActions<Role = Self>
-where
-    Self: Debug,
-{
+#[allow(dead_code)]
+pub trait MqttActions<Role = Self> {
     fn register_action(self) -> Self;
 
     fn log_action(self) -> Self;
