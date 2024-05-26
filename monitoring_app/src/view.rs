9eaf37d96e05aabbc42b3b6134cde4d660127555
tin-gprieto <mgonzalezp@fi.uba.ar>
2024-05-26 06:23:24 -0300

    add: cams table

--- monitoring_app/src/view.rs
+++ monitoring_app/src/view.rs
@@ -1,24 +1,26 @@
 pub mod view{
-    use app::shared::{cam_list::CamList, coordenates::Coordenates, incident::{Incident, IncidentState}};
-    use eframe::egui::{self, Context, Margin};
-    use walkers::{Tiles, Map, MapMemory, Position, sources::OpenStreetMap};
+    use app::shared::{cam_list::{CamList, CamState}, coordenates::Coordenates, incident::{Incident, IncidentState}};
+    use eframe::egui::{self, Margin};
+    use egui_extras::{TableBuilder, Column};
+    /* use walkers::{Tiles, MapMemory, sources::OpenStreetMap}; */
 
     pub struct MyApp {
         system: CamList,
         incident: Vec<Incident>,
-        tiles: Tiles,
-        map_memory: MapMemory,
+        /* tiles: Tiles,
+        map_memory: MapMemory, */
     }
     
     impl MyApp {
-        fn new(egui_ctx: Context) -> Self {
+
+        fn new() -> Self {
             let system = CamList::generate_ramdoms_cams(10);
 
             Self {
                 system,
                 incident: Vec::new(),
-                tiles: Tiles::new(OpenStreetMap, egui_ctx),
-                map_memory: MapMemory::default(),
+                /* tiles: Tiles::new(OpenStreetMap, egui_ctx),
+                map_memory: MapMemory::default(), */
             }
         }
         pub fn add_incident(&mut self, latitud: f64, longitud: f64) {
@@ -41,6 +43,8 @@ pub mod view{
         }
         res
     }
+    
+    
 
     impl eframe::App for MyApp {
 
@@ -49,18 +53,57 @@ pub mod view{
         
         let (mut latitud, mut longitud) = (0.0,0.0);
 
-        let central_frame = egui::Frame {
-            inner_margin: Margin { left: 400.0, ..Default::default()},
-            fill: ctx.style().visuals.panel_fill,
-            ..Default::default()
-        };
-
         let side_frame = egui::Frame {
-            fill: ctx.style().visuals.panel_fill,
+            inner_margin: Margin { left: 30.0, ..Default::default() },
             ..Default::default()
-        };
+        }; 
 
-        egui::SidePanel::left("sidebar").frame(side_frame).show(ctx, |ui| {
+        egui::SidePanel::left("menu").frame(side_frame).show(ctx, |ui| {
+            ui.heading("Listado de cámaras");
+            TableBuilder::new(ui)
+                .column(Column::auto_with_initial_suggestion(50.0))
+                .column(Column::auto_with_initial_suggestion(150.0))
+                .column(Column::auto_with_initial_suggestion(600.0))
+                .column(Column::auto_with_initial_suggestion(600.0))
+                .header(20.0, |mut header| {
+                    header.col(|ui| {
+                        ui.heading("ID");
+                    });
+                    header.col(|ui| {
+                        ui.heading("Estado");
+                    });
+                    header.col(|ui| {
+                        ui.heading("Latitud");
+                    });
+                    header.col(|ui| {
+                        ui.heading("Longitud");
+                    });
+                })
+                .body(|mut body| {
+                    for cam in &self.system.cams {
+                        body.row(20.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label(&format!("{}", cam.id));
+                            });
+                            row.col(|ui| {
+                                if CamState::Alert == cam.state {
+                                    ui.label(egui::RichText::new("Alerta").color(egui::Color32::RED));
+                                } else {
+                                    ui.label(egui::RichText::new("Ahorro de energía").color(egui::Color32::GREEN));
+                                }
+                            });
+                            row.col(|ui| {
+                                ui.label(&format!("{}", cam.location.latitude));
+                            });
+                            row.col(|ui| {
+                                ui.label(&format!("{}", cam.location.longitude));
+                            });
+                        });
+                    }
+                });
+        });
+        
+        egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Carga de incidentes");
             ui.horizontal(|ui| {
                 let name_label = ui.label("Nueva latitud: ");
@@ -73,28 +116,19 @@ pub mod view{
             if ui.button("Agregar incidente").clicked() {
                 self.add_incident(latitud, longitud);
             }
-
+            
             ui.separator();
 
-            ui.heading("Cámaras del sistema");
-            for cam in &self.system.cams {
-                ui.horizontal(|ui| {
-                    ui.label(format!("Camara id: {}", cam.id));
-                    ui.label(format!("Latitud: {}", cam.location.latitude));
-                    ui.label(format!("Longitud: {}", cam.location.longitude));
-                    ui.label(format!("Estado: {}", cam.state));
-                });
-            }
+            
         });
-
-        egui::CentralPanel::default().frame(central_frame).show(ctx, |ui| {
+        /* egui::CentralPanel::default().frame(central_frame).show(ctx, |ui| {
             ui.heading("Apliación de monitoreo");
             ui.add(Map::new(
                 Some(&mut self.tiles),
                 &mut self.map_memory,
                 Position::from_lon_lat(17.03664, 51.09916)
             ));
-        });
+        }); */
     });
     
     });
@@ -104,14 +138,16 @@ pub mod view{
     pub fn run_interface() -> Result<(), eframe::Error> {
 
         let options = eframe::NativeOptions{
-            viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+            viewport: egui::ViewportBuilder::default().with_fullsize_content_view(true),
             ..Default::default()
         };
 
         eframe::run_native(
             "Apliación de monitoreo",
             options,
-            Box::new(|cc| Box::new(MyApp::new(cc.egui_ctx.clone()))),
+            Box::new(|_cc|
+                Box::new(MyApp::new()
+            )),
         )
     }
 }
