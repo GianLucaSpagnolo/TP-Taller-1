7a28025252929887345e185d6e628b0934c28028
tin-gprieto <mgonzalezp@fi.uba.ar>
2024-05-26 14:01:10 -0300

    feat: finish view

--- app/src/shared/cam_list.rs
+++ app/src/shared/cam_list.rs
@@ -38,6 +38,7 @@ impl Display for Cam {
     }
 }
 
+#[derive(Default)]
 pub struct CamList {
     pub cams: Vec<Cam>,
 }
--- monitoring_app/src/view.rs
+++ monitoring_app/src/view.rs
@@ -1,12 +1,14 @@
 pub mod view{
     use app::shared::{cam_list::{CamList, CamState}, coordenates::Coordenates, incident::{Incident, IncidentState}};
     use eframe::egui::{self, Margin};
-    use egui_extras::{TableBuilder, Column};
+    use egui_extras::{Column, TableBuilder};
     /* use walkers::{Tiles, MapMemory, sources::OpenStreetMap}; */
 
+    #[derive(Default)]
     pub struct MyApp {
-        system: CamList,
-        incident: Vec<Incident>,
+        pub system: CamList,
+        pub incident: Vec<Incident>,
+        pub coordenates: Coordenates,
         /* tiles: Tiles,
         map_memory: MapMemory, */
     }
@@ -19,17 +21,18 @@ pub mod view{
             Self {
                 system,
                 incident: Vec::new(),
+                coordenates: Coordenates::default(),
                 /* tiles: Tiles::new(OpenStreetMap, egui_ctx),
                 map_memory: MapMemory::default(), */
             }
         }
-        pub fn add_incident(&mut self, latitud: f64, longitud: f64) {
+        pub fn add_incident(&mut self, location: Coordenates) {
+            
+            println!("Incidente agregado en latitud: {}, longitud: {}", location.latitude, location.longitude);
+            
             self.incident.push(Incident {
                 id: self.incident.len().to_string(),
-                location: Coordenates {
-                    latitude: latitud,
-                    longitude: longitud,
-                },
+                location,
                 state: IncidentState::InProgess,
             });
         }
@@ -44,103 +47,99 @@ pub mod view{
         res
     }
     
-    
-
     impl eframe::App for MyApp {
 
+        fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
-        let (mut latitud, mut longitud) = (0.0,0.0);
+            let frame = egui::Frame {
+                inner_margin: Margin {
+                    top: 30.0,
+                    bottom: 30.0,
+                    left: 30.0,
+                    right: 30.0,
+                },
+                ..Default::default()
+            };
 
-        let side_frame = egui::Frame {
-            inner_margin: Margin { left: 30.0, ..Default::default() },
-            ..Default::default()
-        }; 
-
-        egui::SidePanel::left("menu").frame(side_frame).show(ctx, |ui| {
-            ui.heading("Listado de cámaras");
-            TableBuilder::new(ui)
-                .column(Column::auto_with_initial_suggestion(50.0))
-                .column(Column::auto_with_initial_suggestion(150.0))
-                .column(Column::auto_with_initial_suggestion(600.0))
-                .column(Column::auto_with_initial_suggestion(600.0))
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.heading("ID");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Estado");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Latitud");
-                    });
-                    header.col(|ui| {
-                        ui.heading("Longitud");
-                    });
-                })
-                .body(|mut body| {
-                    for cam in &self.system.cams {
-                        body.row(20.0, |mut row| {
-                            row.col(|ui| {
-                                ui.label(&format!("{}", cam.id));
-                            });
-                            row.col(|ui| {
-                                if CamState::Alert == cam.state {
-                                    ui.label(egui::RichText::new("Alerta").color(egui::Color32::RED));
-                                } else {
-                                    ui.label(egui::RichText::new("Ahorro de energía").color(egui::Color32::GREEN));
-                                }
-                            });
-                            row.col(|ui| {
-                                ui.label(&format!("{}", cam.location.latitude));
-                            });
-                            row.col(|ui| {
-                                ui.label(&format!("{}", cam.location.longitude));
-                            });
-                        });
-                    }
+            egui::CentralPanel::default().frame(frame).show(ctx,  |ui| {
+                ui.heading("Carga de incidentes");
+                ui.horizontal(|ui| {
+                    let name_label = ui.label("Nueva latitud: ");
+                    integer_edit_field(ui, &mut self.coordenates.latitude)
+                    .labelled_by(name_label.id);
                 });
-        });
+                ui.horizontal(|ui| {
+                    let name_label = ui.label("Nueva longitud: ");
+                    integer_edit_field(ui,&mut self.coordenates.longitude)
+                    .labelled_by(name_label.id);
+                });
+                if ui.button("Agregar incidente").clicked() {
+                    self.add_incident(self.coordenates.clone());
+                }
+                ui.separator();
+                
+                ui.heading("Listado de cámaras");
+                TableBuilder::new(ui)
+                    .column(Column::exact(50.0))
+                    .column(Column::exact(250.0))
+                    .column(Column::exact(250.0))
+                    .column(Column::exact(250.0))
+                    .header(30.0, |mut header| {
+                        header.col(|ui| {
+                            ui.heading("ID");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Estado");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Latitud");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Longitud");
+                        });
+                    })
+                    .body(|mut body| {
+                        for cam in &self.system.cams {
+                            body.row(20.0, |mut row| {
+                                row.col(|ui| {
+                                    ui.label(&format!("{}", cam.id));
+                                });
+                                row.col(|ui| {
+                                    if CamState::Alert == cam.state {
+                                        ui.label(egui::RichText::new("Alerta").color(egui::Color32::RED));
+                                    } else {
+                                        ui.label(egui::RichText::new("Ahorro de energía").color(egui::Color32::GREEN));
+                                    }
+                                });
+                                row.col(|ui| {
+                                    ui.label(&format!("{}", cam.location.latitude));
+                                });
+                                row.col(|ui| {
+                                    ui.label(&format!("{}", cam.location.longitude));
+                                });
+                            });
+                        }
+                    });
+            });
         
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Carga de incidentes");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Nueva latitud: ");
-                integer_edit_field(ui, &mut latitud)
-                .labelled_by(name_label.id);
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Nueva longitud: ");
-                integer_edit_field(ui, &mut longitud)
-                .labelled_by(name_label.id);
-            if ui.button("Agregar incidente").clicked() {
-                self.add_incident(latitud, longitud);
-            }
-            
-            ui.separator();
+            /* egui::CentralPanel::default().frame(central_frame).show(ctx, |ui| {
+                ui.heading("Apliación de monitoreo");
+                ui.add(Map::new(
+                    Some(&mut self.tiles),
+                    &mut self.map_memory,
+                    Position::from_lon_lat(17.03664, 51.09916)
+                ));
+            }); */
+    }   
 
             
-        });
-        /* egui::CentralPanel::default().frame(central_frame).show(ctx, |ui| {
-            ui.heading("Apliación de monitoreo");
-            ui.add(Map::new(
-                Some(&mut self.tiles),
-                &mut self.map_memory,
-                Position::from_lon_lat(17.03664, 51.09916)
-            ));
-        }); */
-    });
-    
-    });
-    }
     }
 
     pub fn run_interface() -> Result<(), eframe::Error> {
 
-        let options = eframe::NativeOptions{
-            viewport: egui::ViewportBuilder::default().with_fullsize_content_view(true),
-            ..Default::default()
-        };
+        let mut options = eframe::NativeOptions::default();
+
+        options.centered = true;
 
         eframe::run_native(
             "Apliación de monitoreo",
