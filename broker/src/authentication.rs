99a4c8490d5e727e6a015ed5af20ef4e608b0924
GianLucaSpagnolo <gianluuba@gmail.com>
2024-06-24 03:50:22 -0300

    add: broker clients identified

--- broker/src/authentication.rs
+++ broker/src/authentication.rs
@@ -1,35 +1,65 @@
+use mqtt::common::utils::*;
 use std::{
     fs::File,
-    io::Error,
+    io::{BufRead, BufReader, Error},
 };
-use mqtt::common::utils::*;
 
 pub struct AuthData {
     pub users: Vec<String>,
-    pub password: String,
+}
+
+fn read_config_file(file: &File) -> Option<Vec<String>> {
+    let mut lines = Vec::new();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            Ok(linea) => lines.push(linea),
+            Err(e) => {
+                eprintln!("Error al leer linea: {}", e);
+                return None;
+            }
+        }
+    }
+
+    Some(lines)
 }
 
 impl AuthData {
     pub fn from_file(file_path: String) -> Result<Self, Error> {
         let mut users = Vec::new();
-        let mut password = String::new();
-        let open_file: Option<File> = open_config_file(&file_path);
 
-        open_file.map(|archivo| match read_config_file(&archivo) {
-            None => None,
-            Some(lineas_leidas) => {
-                let parametros = get_file_parameters(lineas_leidas, 2);
-                for (key, value) in parametros {
-                    match key.as_str() {
-                        "users" => users = value.split(',').map(|s| s.to_string()).collect(),
-                        "password" => password = value,
-                        _ => (),
+        let open_file: Option<File> = open_config_file(&file_path);
+        match open_file {
+            None => {
+                return Err(Error::new(
+                    std::io::ErrorKind::NotFound,
+                    "No se encontro el archivo",
+                ))
+            }
+            Some(_) => (),
+        }
+
+        let file = read_config_file(&open_file.unwrap());
+        match file {
+            None => {
+                return Err(Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Error al leer el archivo",
+                ))
+            }
+            Some(lines) => {
+                let mut is_usernames = true;
+                for (i, line) in lines.into_iter().enumerate() {
+                    if i == 0 && line == "broker_registered_apps: " {
+                        is_usernames = true;
+                    } else if is_usernames {
+                        users.push(line);
                     }
                 }
-                Some(())
             }
-        });
+        }
 
-        Ok(AuthData { users, password })
+        Ok(AuthData { users })
     }
 }
--- broker/src/broker.rs
+++ broker/src/broker.rs
@@ -1,3 +1,4 @@
+use broker::authentication::AuthData;
 use logger::logger_handler::create_logger_handler;
 use mqtt::{
     config::{mqtt_config::Config, server_config::ServerConfig},
@@ -13,6 +14,14 @@ fn main() -> ExitCode {
     const CONFIGERROR: u8 = 3;
     const SERVER_LISTENERERROR: u8 = 4;
     const LOGGER_INITERROR: u8 = 5;
+
+    let auth_data = match AuthData::from_file(String::from(AUTH_DATA_PATH)) {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("Error al obtener datos de autenticacion: {}", e);
+            return CONFIGERROR.into();
+        }
+    };
 
     let config = match ServerConfig::from_file(String::from(CONFIG_PATH)) {
         Ok(conf) => conf,
@@ -40,7 +49,7 @@ fn main() -> ExitCode {
         }
     };
 
-    match MqttServer::new(config).start_server(logger.get_logger()) {
+    match MqttServer::new(config, auth_data.users).start_server(logger.get_logger()) {
         Ok(_) => {
             logger.close();
             0.into()
